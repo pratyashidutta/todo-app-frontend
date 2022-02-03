ea25613fc94c4ba574abdb3335caec7a21c8f120
Pratyashi_Dutta <Pratyashi_Dutta@Dell.com>
2022-02-04 00:28:47 +0530

    adding logout feature in menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -12,20 +13,25 @@ export class LoginComponent implements OnInit {
   password=""
   errorMessage="Invalid credentials"
   invalidLogin=false
-  constructor(private router: Router) { }
+  constructor(private router: Router, private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
   handleLogin(){
     // console.log('Hi',this.username+" "+this.password);
-    if (this.username=="12345" && this.password=="abcde"){
+    // if (this.username=="12345" && this.password=="abcde"){
+      
+      if(this.hardcodedAuthenticationService.authenticate(this.username,this.password)){
       this.invalidLogin=false
       this.router.navigate(['welcome',this.username])
+      
+      
+      }
       // this.invalidLogin=true
-    }
+    // }
     else{
       this.invalidLogin=true
     }
-
+    
   }
 }
--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class HardcodedAuthenticationService {
 
   constructor() { }
+  authenticate(username, password){
+    // console.log("Before "+this.isUserLoggedIn())
+    if (username=="12345" && password=="abcde"){
+      sessionStorage.setItem('authenticatedUser', username)
+      // console.log("After "+this.isUserLoggedIn())
+      return true
+    }
+    return false
+  }
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    console.log(user)
+    return !(user==null)
+  }
+  isUserLoggedOut(){
+    sessionStorage.removeItem('authenticatedUser')
+  }
 }
