116db6f849ef14cdda4baaed3f4d666a83de7b4f
Pratyashi_Dutta <Pratyashi_Dutta@Dell.com>
2022-02-13 20:14:05 +0530

    logout feature

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
